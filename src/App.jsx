4ca4dc95310962617ac44c0728b07b901f8d5c3a
Ikechukwu Kalu <iyke4web@gmail.com>
2024-02-09 01:06:48 +0100

    Building Category Page

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ import {
   Login,
   Verification,
   User,
+  Categories,
 } from "./pages/Main";
 import { Routes, Route } from "react-router-dom";
-import  Header from "./components/Header";
+import Header from "./components/Header";
 import { Popup } from "./components/Popup";
 import { FindASchool } from "./pages/FindASchool";
 import { Parent } from "./pages/Parent";
@@ -17,21 +18,21 @@ import { Parent } from "./pages/Parent";
 function App() {
   return (
     <>
-    <GlobalStyles />
-    <Header />
-    <Container>
-    <Routes>
-         <Route path="/"element={<Home/>} />
-         <Route path="/About"element={<About/>} />
-         <Route path="/Contact"element={<Contact/>} />
-         <Route path="/login" element={<Login />} />
-         <Route path="/popup" element={<Popup />} />
-         <Route path="/Parent" element={<Parent />}/>
-         <Route path="/verification" element={<Verification />} />
-         <Route path="/FindASchool" element={<FindASchool/>}/>
-         <Route path="/User" element={<User />}/>
-         <Route path="*" element={<h1>Page Not Found</h1>} />
-    
+      <GlobalStyles />
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/popup" element={<Popup />} />
+          <Route path="/Parent" element={<Parent />} />
+          <Route path="/verification" element={<Verification />} />
+          <Route path="/FindASchool" element={<FindASchool />} />
+          <Route path="/User" element={<User />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
         </Routes>
       </Container>
     </>
