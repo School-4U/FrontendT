ac37b3abe1d7512dd3b1612afbde051edb194bd0
lamidimorufat <lamidimorufat0@gmail.com>
2024-02-09 01:15:08 +0100

    added meet the team to the about page

--- src/components/styles/About.styled.jsx
+++ src/components/styles/About.styled.jsx
@@ -158,6 +158,23 @@ p{
     }
 }
 `
+export const Team = styled.div`
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+gap: 10px;
+
+@media screen and (min-width: 768px) and (max-width: 1200px){
+grid-template-columns: repeat(3, 1fr);
+gap: 10px;
+       
+}
+
+@media screen and (min-width: 320px) and (max-width: 768px){
+    grid-template-columns: repeat(2, 1fr);
+    gap: 10px;
+}
+
+`
 export const Mission = styled.div`
 margin-top: 30px;
 background-image: url("assets/Mission.jpg");
