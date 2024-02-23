d72d3d0297b2c783856a04e928ac3c3c2695c945
Ikechukwu Kalu <71293780+Ikechukwu750@users.noreply.github.com>
2024-02-23 06:52:33 +0100

    Update AuthContext.jsx

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useContext } from 'react';
 
-const AuthContext = createContext();
+const AuthContext = createContext();  
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
